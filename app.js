67f2b45af03fc0e75428fb8aafe78850b3f7efce
faridEsnaashari <faride13578642@gmail.com>
2020-04-29 00:03:41 +0430

    add routes process

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    next();
-});
+const userRoute = require('./api/routes/user');
+const gamestatusRoute = require('./api/routes/gamestatus');
+const questionRoute = require('./api/routes/question');
+const storeRoute = require('./api/routes/store');
+
+app.use('/user', userRoute);
+app.use('/gamestatus', gamestatusRoute);
+app.use('/question', questionRoute);
+app.use('/store', storeRoute);
 
 module.exports = app;
