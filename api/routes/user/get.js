25a7265c25f6887f4a6d0f341e29f900b0385d6e
faridEsnaashari <faride13578642@gmail.com>
2020-05-04 14:15:55 +0430

    add 404 and 200 response

--- api/routes/user/get.js
+++ api/routes/user/get.js
@@ -1,25 +1,49 @@
-const responseGenerator = require('../../responseGenerator.js');
+const responseGenerator = require('../../responseGenerator');
+const sqlConnection = require('../../sqlConnection');
+
 function handleGetRequest(req, res)
 {
     if(!checkParameters(req))
     {
-        responseJson = {
+        const responseJson = {
             message : "bad parameter provided"
         };
-        responseGenerator(res, 400, responseJson)
+        responseGenerator(res, 400, responseJson);
 
         return;
     }
-    let user = {
-        username : undefined,
-        phonenumber : undefined,
-        name : undefined,
-        teamColor : undefined,
-        health : undefined,
-        ticket : undefined,
-        score : undefined
+
+    let query = null;
+    if(req.query.phonenumber != undefined){
+        query = "SELECT * FROM user where phonenumber = '" + req.query.phonenumber + "'";
     }
-    responseGenerator(res, 200, user)
+    else{
+        query = "SELECT * FROM user where username = '" + req.query.username + "'";
+    }
+
+    sqlConnection.query(query, function (err, result, fields) {
+        if (err) throw err;
+
+        if(result[0] == undefined)
+        {
+            const responseJson = {
+              "message": "user not found"
+            };
+            responseGenerator(res, 404, responseJson);
+            return;
+        }
+        const user = {
+            username : result[0].username,
+            phonenumber : result[0].phonenumber,
+            name : result[0].name,
+            teamColor : result[0].teamColor,
+            health : result[0].health,
+            ticket : result[0].ticket,
+            score : result[0].score
+        }
+        responseGenerator(res, 200, user);
+    });
+
 }
 
 function checkParameters(req)
