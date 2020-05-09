8d5934b728eee15074eb8eaa5868063dbae6beff
faridEsnaashari <faride13578642@gmail.com>
2020-05-10 01:25:32 +0430

    add put request handler

--- api/routes/user/user.js
+++ api/routes/user/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const handleGetRequest = require('./get');
 const handlePostRequest = require('./post');
-//const handlePutRequest = require('./put');
+const handlePutRequest = require('./put');
 //const handleDeleteRequest = require('./delete');
 
 router.get('/', (req, res, next) => {
@@ -15,12 +15,12 @@ router.post('/', (req, res, next) => {
     handlePostRequest(req, res);
 });
 
-router.delete('/', (req, res, next) => {
-    //handleDeleteRequest(req, res);
+router.put('/', (req, res, next) => {
+    handlePutRequest(req, res);
 });
 
-router.put('/', (req, res, next) => {
-    //handlePutRequest(req, res);
+router.delete('/', (req, res, next) => {
+    //handleDeleteRequest(req, res);
 });
 
 
