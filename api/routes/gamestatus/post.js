e4fa8ccc4ed7848d3c0115db1f1e92fa2e34acf8
faridEsnaashari <faride13578642@gmail.com>
2020-05-10 15:12:26 +0430

    add 201 and 400 response for post

---
+++ api/routes/gamestatus/post.js
@@ -0,0 +1,48 @@
+const responseGenerator = require('../../responseGenerator');
+const sqlConnection = require('../../sqlConnection');
+
+function handlePostRequest(req, res)
+{
+    if(!checkParameters(req))
+    {
+        const responseJson = {
+            message : "bad parameter provided"
+        };
+        responseGenerator(res, 400, responseJson);
+
+        return;
+    }
+
+    const query = "insert into gamestatus(label, startedtime, duration)" + 
+        ' value (?)';
+    const data = [
+        req.body.label,
+        req.body.startedtime,
+        req.body.duration
+    ];
+
+    sqlConnection.query(query, [data], function (err, result, fields) {
+        if (err) throw err;
+
+        responseJson = {
+              "message": "game created"
+        };
+        responseGenerator(res, 201, responseJson);
+    });
+
+}
+
+function checkParameters(req)
+{
+    if(req.body.label == undefined || req.body.label == null ||
+        req.body.startedtime == undefined || req.body.startedtime == null ||
+        req.body.duration == undefined || req.body.duration == null
+    )
+    {
+        return false;
+    }
+    return true;
+}
+
+
+module.exports = handlePostRequest;
