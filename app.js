5b6373371179a7e705ee8778a379c44ed5d164de
faridEsnaashari <faride13578642@gmail.com>
2020-06-24 12:32:51 +0430

    add pay route

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const userRoute = require('./api/routes/user/user');
 const gamestatusRoute = require('./api/routes/gamestatus/gamestatus');
 const questionRoute = require('./api/routes/question/question');
 const storeRoute = require('./api/routes/store/store');
+const payRoute = require('./api/routes/pay/pay');
+
 
 app.use('/user', userRoute);
 app.use('/gamestatus', gamestatusRoute);
 app.use('/question', questionRoute);
 app.use('/store', storeRoute);
+app.use('/pay', payRoute);
 
 module.exports = app;
