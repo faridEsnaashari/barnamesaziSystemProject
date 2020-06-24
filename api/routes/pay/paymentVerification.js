754d00c1d8665422fbe7db8588d976008cd7582c
faridEsnaashari <faride13578642@gmail.com>
2020-06-24 12:34:35 +0430

    handle verificaiton of payment

---
+++ api/routes/pay/paymentVerification.js
@@ -0,0 +1,52 @@
+const responseGenerator = require('../../responseGenerator');
+const zarinpal = require('../../zarinpal');
+const token = require('../../token');
+const sqlConnection = require('../../sqlConnection');
+
+function handlePaymentVerificationRequest(req, res){
+
+    const requestStatus = req.query.Status;
+    if(requestStatus !== 'OK'){
+        const responseJson = {
+            message : 'payment process failed'
+        };
+        responseGenerator(res, 406, responseJson);
+    }
+
+    const amount = req.query.amount;
+    const userId = req.query.userid;
+    const authority = req.query.Authority;
+
+    const callback = (refid) => {
+        if(refid === 0){
+            const responseJson = {
+                message : 'payment process failed'
+            };
+            responseGenerator(res, 406, responseJson);
+            return;
+        }
+
+        const query = "insert into transactions(userid, refid, amount)" + 
+            ' value (?)';
+        const data = [
+            userId,
+            refid,
+            amount
+        ];
+
+        sqlConnection.query(query, [data], function (err, result, fields) {
+            if (err) throw err;
+
+            const responseJson = {
+                RefID : refid,
+                message : 'payment accepted'
+            };
+            responseGenerator(res, 200, responseJson);
+        });
+        return;
+    }
+
+    zarinpal.verify(amount, authority, callback);
+}
+module.exports = handlePaymentVerificationRequest;
+
