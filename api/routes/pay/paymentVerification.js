3f0a756ddad328cc6365f4e84a110d399640eae6
faridEsnaashari <faride13578642@gmail.com>
2020-07-14 20:29:07 +0430

    get callback out of main function

--- api/routes/pay/paymentVerification.js
+++ api/routes/pay/paymentVerification.js
@@ -10,42 +10,45 @@ function handlePaymentVerificationRequest(req, res){
             message : 'payment process failed'
         };
         responseGenerator(res, 406, responseJson);
+        return;
     }
 
     const amount = req.query.amount;
     const userId = req.query.userid;
     const authority = req.query.Authority;
 
-    const callback = (refid) => {
-        if(refid === 0){
-            const responseJson = {
-                message : 'payment process failed'
-            };
-            responseGenerator(res, 406, responseJson);
-            return;
-        }
 
-        const query = "insert into transactions(userid, refid, amount)" + 
-            ' value (?)';
-        const data = [
-            userId,
-            refid,
-            amount
-        ];
+    zarinpal.verify(res, userId, amount, authority, callback);
+}
 
-        sqlConnection.query(query, [data], function (err, result, fields) {
-            if (err) throw err;
-
-            const responseJson = {
-                RefID : refid,
-                message : 'payment accepted'
-            };
-            responseGenerator(res, 200, responseJson);
-        });
+const callback = (res, refid, userId, amount) => {
+    if(refid === "failed"){
+        const responseJson = {
+            message : 'payment process failed'
+        };
+        responseGenerator(res, 406, responseJson);
         return;
     }
 
-    zarinpal.verify(amount, authority, callback);
+    const query = "insert into transactions(userid, refid, amount)" + 
+        ' value (?)';
+    const data = [
+        userId,
+        refid,
+        amount
+    ];
+
+    sqlConnection.query(query, [data], function (err, result, fields) {
+        if (err) throw err;
+
+        const responseJson = {
+            RefID : refid,
+            message : 'payment accepted'
+        };
+        responseGenerator(res, 200, responseJson);
+
+        return;
+    });
 }
 module.exports = handlePaymentVerificationRequest;
 
