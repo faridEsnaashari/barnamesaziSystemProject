9813afe8c20201a4df5ec0134ca40c7517056fb2
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 18:58:22 +0430

    change pay process

--- api/zarinpal.js
+++ api/zarinpal.js
@@ -2,35 +2,32 @@ const zarinpalCheckout = require('zarinpal-checkout');
 
 const zarinpal = zarinpalCheckout.create('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', true);
 
-function pay(res, amount, userId){
+function pay(res, amount, userId, callback, itemId){
     zarinpal.PaymentRequest({
         Amount: amount,
-        CallbackURL: 'http://localhost:3000/pay/verify?userid=' + userId + '&amount=' + amount,
+        CallbackURL: 'localhost:3000/pay/verify?userid=' + userId + '&amount=' + amount + '&itemid=' + itemId,
         Description: 'pay for the game'
-    }).then(function (response) {
-        if (response.status == 100) {
-            res.redirect(response.url);
-        }
-
+    }).then(function(response){
+        callback(response, res);
     }).catch(function (err) {
-        console.log(err);
+        throw err;
     });
 }
 
-function verify(amount, authority, callback){
+function verify(itemId, res, userId, amount, authority, callback){
     zarinpal.PaymentVerification({
     Amount: amount,
     Authority: authority,
 
     }).then(function (response) {
-        if (response.status == 100) {
+        if (response.status == 100 || response.status == 101) {
             return response.RefID;
         } 
         else {
             return "failed";
         }
     }).then(function (result){
-        callback(result);
+        callback(res, result, userId, amount, itemId);
     }).catch(function (err) {
         throw err;
     });
