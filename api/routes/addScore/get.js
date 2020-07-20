dc16a689d69f7180b0931b019529f2117a397a0d
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 19:02:07 +0430

    handle get requests for addscore route

---
+++ api/routes/addScore/get.js
@@ -0,0 +1,83 @@
+const responseGenerator = require('../../responseGenerator');
+const sqlConnection = require('../../sqlConnection');
+const token = require('../../token');
+
+function handleGetRequest(req, res)
+{
+    if(!checkParameters(req))
+    {
+        const responseJson = {
+            message : "bad parameter provided"
+        };
+        responseGenerator(res, 400, responseJson);
+
+        return;
+    }
+
+    const score = req.query.score;
+    const userToken = req.query.usertoken;
+
+    let userId = null;
+    try{
+        userId = token.verify(userToken);
+    }
+    catch(err){
+        const responseJson = {
+            message : "user unauthorized"
+        };
+        responseGenerator(res, 401, responseJson);
+
+        return;
+    }
+
+    const updateUserScoreQuery = "update user set score = score + " + score + " where userId = " + userId;
+ 
+    sqlConnection.query(updateUserScoreQuery, function (err, result, fields) {
+        if (err) throw err;
+
+        const getActiveGameQuery = "select id from gamestatus where active = true";
+        sqlConnection.query(getActiveGameQuery, function (err, result, fields) {
+            if(err) throw err;
+
+            activeGameId = result[0].id;
+            const checkTableQuery = "select * from activegamescore where gameId = " + activeGameId + " and userId = " + userId; 
+            sqlConnection.query(checkTableQuery, function (err, result, fields) {
+                if(err) throw err;
+
+                let changeScoreQuery = ""; 
+                if(result[0] == undefined){
+                    changeScoreQuery = "insert into activegamescore value(" + userId + ", " + activeGameId + ", " + score + ")";
+                }
+                else{
+                    changeScoreQuery = "update activegamescore set score = score +" + score + " where gameId = " + activeGameId + " and userId = " + userId;
+                }
+                sqlConnection.query(changeScoreQuery, function (err, result, fields) {
+                    if(err) throw err;
+                    const responseJson = {
+                        message : "user score updated"
+                    };
+                    responseGenerator(res, 200, responseJson);
+
+                    return;
+                });
+            });
+        });
+    });
+
+}
+
+function checkParameters(req)
+{
+//    if(req.query.phonenumber == undefined && req.query.username == undefined)
+//    {
+//        return false;
+//    }
+//    return true;
+    if(req.query.usertoken == undefined || req.query.score == undefined)
+    {
+        return false;
+    }
+    return true;
+}
+
+module.exports = handleGetRequest;
