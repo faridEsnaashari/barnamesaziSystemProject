d682efea9702982f88097f8d34b7ecc97a6bf6d6
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 17:43:29 +0430

    now can insert more than one question

--- api/routes/question/post.js
+++ api/routes/question/post.js
@@ -13,39 +13,52 @@ function handlePostRequest(req, res)
         return;
     }
 
-    const query = "insert into question(question, answer1, answer2, answer3, answer4, currect)" + 
-        ' value (?)';
-    const data = [
-        req.body.question,
-        req.body.answer1,
-        req.body.answer2,
-        req.body.answer3,
-        req.body.answer4,
-        req.body.currect
-    ];
+    const query = generateInsertQuery(req);
 
-    sqlConnection.query(query, [data], function (err, result, fields) {
+    sqlConnection.query(query, function (err, result, fields) {
         if (err) throw err;
 
         responseJson = {
-              "message": "question created"
+              "message": "question(s) created"
         };
         responseGenerator(res, 201, responseJson);
     });
 
 }
 
+function generateInsertQuery(req){
+    let query = "insert into question(question, answer1, answer2, answer3, answer4, currect) values ";
+    for(question in req.body.questions){
+        const eachQuestion = req.body.questions[question];
+        query += "(\'" + eachQuestion.question + "\', \'" + eachQuestion.answer1 + "\', \'" + eachQuestion.answer2 + "\', \'" + eachQuestion.answer3 + "\', \'" + eachQuestion.answer4 + "\', " +eachQuestion.currect + "),";
+    }
+    query = query.slice(0, query.length-1);
+    return query;
+}
+
 function checkParameters(req)
 {
-    if(req.body.question == undefined || req.body.question == null ||
-        req.body.answer1 == undefined || req.body.answer1 == null ||
-        req.body.answer2 == undefined || req.body.answer2 == null ||
-        req.body.answer3 == undefined || req.body.answer3 == null ||
-        req.body.answer4 == undefined || req.body.answer4 == null ||
-        req.body.currect == undefined || req.body.currect == null
-    )
-    {
+    if(req.body.questions == undefined || req.body.questions == null){
         return false;
+    }
+    for(let question in req.body.questions){
+        if(Object.keys(req.body.questions[question]).length < 1){
+            return false;
+        }
+        else{
+            const eachQuestion = req.body.questions[question];
+            if(
+                (eachQuestion.question == undefined || eachQuestion.question == null)||
+                (eachQuestion.answer1 == undefined || eachQuestion.answer1 == null)||
+                (eachQuestion.answer2 == undefined || eachQuestion.answer2 == null)||
+                (eachQuestion.answer3 == undefined || eachQuestion.answer3 == null)||
+                (eachQuestion.answer4 == undefined || eachQuestion.answer4 == null)||
+                (eachQuestion.currect == undefined || eachQuestion.currect == null)
+            ){
+                return false;
+            }
+
+        }
     }
     return true;
 }
