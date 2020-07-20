91900e9de626cf585f6b95d34668cfae7f787318
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 18:43:21 +0430

    now can update more than one item

--- api/routes/store/put.js
+++ api/routes/store/put.js
@@ -1,8 +1,7 @@
 const responseGenerator = require('../../responseGenerator');
 const sqlConnection = require('../../sqlConnection');
 
-function handlePutRequest(req, res)
-{
+function handlePutRequest(req, res){
     if(!checkParameters(req))
     {
         const responseJson = {
@@ -14,49 +13,109 @@ function handlePutRequest(req, res)
     }
 
     const query = generateUpdateQuery(req);
+    let queryCounter = query.length - 1;
+    let queryExceptions = [];
 
-    sqlConnection.query(query, function (err, result, fields) {
-        if (err) throw err;
+    executeQuery(query, queryCounter, queryExceptions)
+        .then(function(data){
+            return callback(data)
+        })
+        .then(function(){
+            if(queryExceptions.length > 0){
+                responseJson = {
+                    "message" : "these/this item(s) not found",
+                    items : queryExceptions
+                }
+                responseGenerator(res, 409, responseJson);
+            }
+            else{
+                responseJson = {
+                    "message": "item updated"
+                };
+                responseGenerator(res, 200, responseJson);
+            }
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+}
 
-        if(result.affectedRows === 0){
-            responseJson = {
-                "message" : "item not found"
-            };
-            responseGenerator(res, 404, responseJson);
-            return;
-        }
-        responseJson = {
-              "message": "item updated"
-        };
-        responseGenerator(res, 200, responseJson);
+function callback(data){
+    if(data.result.affectedRows === 0){
+        data.queryExceptions.push(getItemIdFromQuery(data.query[data.queryCounter]));
+    }
+    data.queryCounter--;
+    if(!(data.queryCounter === -1)){
+        return new Promise(function(resolve, reject){
+            executeQuery(data.query, data.queryCounter, data.queryExceptions)
+            .then(function(data){
+                return callback(data)
+            })
+            .then(function(data){
+                resolve(data)
+            })
+            .catch(function(error){
+                console.error(error);
+            });
+        })
+    }
+}
+
+function executeQuery(query, queryCounter, queryExceptions){
+    return new Promise(function(resolve, reject){
+        sqlConnection.query(query[queryCounter], function(err, result, fields){
+            if(err){
+                reject(err);
+            }
+            else{
+                data = {
+                    query : query,
+                    queryCounter : queryCounter,
+                    queryExceptions : queryExceptions,
+                    result : result
+                }
+                resolve(data);
+            }
+        });
     });
-
 }
 
 function generateUpdateQuery(req){
-    let query = null;
-    for(item in req.body){
-        query = "update store " + 
-            "set " + 
-            "count = " + req.body[item].count +
-            ", price = " + req.body[item].price + 
-            " where name = '" + item + "'";
+    let query = [];
+    for(item in req.body.items){
+        eachItem = req.body.items[item];
+        subQuery = "update store set count = " + eachItem.count + ", price = " + eachItem.price + " where id = " + eachItem.id;
+        query.push(subQuery);
     }
     return query;
 }
 
+function getItemIdFromQuery(query){
+    const equalPosition = query.lastIndexOf("=");
+    return query.slice(equalPosition + 2, query.length);
+}
+
+
 function checkParameters(req)
 {
-    for(let item in req.body){
-        if(
-            (req.body[item].count == undefined || req.body[item].count == null) ||
-            (req.body[item].price == undefined || req.body[item].price == null)
-        ){
+    if(req.body.items == undefined || req.body.items == null){
+        return false;
+    }
+    for(let item in req.body.items){
+        if(Object.keys(req.body.items[item]).length < 1){
             return false;
         }
-    }
-    if(Object.keys(req.body).length != 1){
-        return false;
+        else{
+            const eachItem = req.body.items[item];
+            if(
+                (eachItem.id == undefined || eachItem.id == null)||
+                (eachItem.count == undefined || eachItem.count == null)||
+                (eachItem.price == undefined || eachItem.price == null)
+            ){
+                return false;
+            }
+
+        }
     }
     return true;
 }
