fc30578e500219a40503f9ca957f7d4c5f8ac607
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 17:44:20 +0430

    add put and delete handler for question route

--- api/routes/question/question.js
+++ api/routes/question/question.js
@@ -4,6 +4,8 @@ const router = express.Router();
  
 const handleGetRequest = require('./get');
 const handlePostRequest = require('./post');
+const handlePutRequest = require('./put');
+const handleDeleteRequest = require('./delete');
 
 router.get('/', (req, res, next) => {
     handleGetRequest(req, res);
@@ -11,6 +13,14 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
     handlePostRequest(req, res);
+});
+
+router.put('/', (req, res, next) => {
+    handlePutRequest(req, res);
+});
+
+router.delete('/', (req, res, next) => {
+    handleDeleteRequest(req, res);
 });
 
 module.exports = router;
