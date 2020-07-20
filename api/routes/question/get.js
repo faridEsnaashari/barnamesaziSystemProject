c5815b1e7796c5143aaf6d3c918e4de115c59445
faridEsnaashari <faride13578642@gmail.com>
2020-07-20 17:40:37 +0430

    if high and low number provide in query, it send questions between these two number or if don't, int send a question randomlly as response

--- api/routes/question/get.js
+++ api/routes/question/get.js
@@ -3,21 +3,36 @@ const sqlConnection = require('../../sqlConnection');
 
 function handleGetRequest(req, res)
 {
-    const query = "select * from question order by rand() limit 1"
+    let query = [];
+    if(req.query.lownumber != undefined && req.query.highnumber != undefined){
+        const limit = req.query.highnumber - req.query.lownumber;
+        query = "select * from question limit " + limit + " offset " + req.query.lownumber;
+        
+    }
+    else{
+         query = "select * from question order by rand() limit 1";
+    }
 
     sqlConnection.query(query, function (err, result, fields) {
         if (err) throw err;
 
-        const question = {
-            id : result[0].id,
-            question: result[0].question,
-            answer1 : result[0].answer1,
-            answer2 : result[0].answer2,
-            answer3 : result[0].answer3,
-            answer4 : result[0].answer4,
-            currect : result[0].currect
+        let questions = [];
+        for(question in result){
+            const questionRow = {
+                id : result[question].id,
+                question: result[question].question,
+                answer1 : result[question].answer1,
+                answer2 : result[question].answer2,
+                answer3 : result[question].answer3,
+                answer4 : result[question].answer4,
+                currect : result[question].currect
+            };
+            questions.push(questionRow);
+        }
+        const responseJson = {
+            questions : questions
         };
-        responseGenerator(res, 200, question);
+        responseGenerator(res, 200, responseJson);
     });
 
 }
