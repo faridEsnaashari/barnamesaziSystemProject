b09dcbb8859b4f6c0b55039284b4b0b7d03c80a1
faridEsnaashari <faride13578642@gmail.com>
2020-07-27 15:26:19 +0430

    add get method for /dashboard/user route

--- api/routes/admin/admin.js
+++ api/routes/admin/admin.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const handleRoot = require('./root');
 const handlePostLogin = require('./postLogin');
 const handleGetLogin = require('./getLogin');
-const handleDashboard = require('./dashboard/dashboard');
 const handleLogout = require('./logout');
+
+const handleDashboard = require('./dashboard/dashboard');
+const handleGetUser = require('./dashboard/user/userGet');
 
 router.get('/', (req, res, next) => {
     handleRoot(req, res);
@@ -19,12 +21,16 @@ router.post('/login', (req, res, next) => {
     handlePostLogin(req, res);
 });
 
+router.get('/logout', (req, res, next) => {
+    handleLogout(req, res);
+});
+
 router.get('/dashboard', (req, res, next) => {
     handleDashboard(req, res);
 });
 
-router.get('/logout', (req, res, next) => {
-    handleLogout(req, res);
+router.get('/dashboard/user', (req, res, next) => {
+    handleGetUser(req, res);
 });
 
 module.exports = router;
